8964da85ec689031cdd22dea1eeefe7da3d81d5c
jds-codes <agsurjyo@gmail.com>
2026-04-22 23:45:59 +0600

    First commit

---
+++ Demo.py
@@ -0,0 +1,15 @@
+name= "Jyotirmoy Das Surjyo"
+print("My Name is", name)
+myage= 23
+print("I am",myage,"years old")
+fathername= "Topon Das"
+print("My Fathers name is",fathername)
+mothername= "Juthika Ghosh"
+print("My Mothers name is",mothername)
+fatherage=myage+35
+print("My Father is", fatherage, "years old")
+motherage=30+myage
+print("My mother is",motherage,"years old")
+print(type(name))
+print(type(myage))
+print(type(fatherage))
---
+++ readme.md
@@ -0,0 +1,2 @@
+My name is Jyotirmoy Das Surjyo..I am practice some python code for doing well in python..
+In this repo i upload my practice codes.
