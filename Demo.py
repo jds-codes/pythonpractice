f7d1b9be8c17891cf9c3d2876627427067602a03
jds-codes <agsurjyo@gmail.com>
2026-04-24 00:49:37 +0600

    updated

--- Demo.py
+++ Demo.py
@@ -1,43 +1,53 @@
-name= "Jyotirmoy Das Surjyo"
-print("My Name is", name)
-myage= 23
-print("I am",myage,"years old")
-fathername= "Topon Das"
-print("My Fathers name is",fathername)
-mothername= "Juthika Ghosh"
-print("My Mothers name is",mothername)
-fatherage=myage+35
-print("My Father is", fatherage, "years old")
-motherage=30+myage
-print("My mother is",motherage,"years old")
-print(type(name))
-print(type(myage))
-print(type(fatherage))
-num=5
-num2=6
-print(num % num2) #remainder
-print(num ** num2) #power
-print(num / num2)
-print(num == num2) #equal
-print(num != num2) #not equal
-print(num)
-print (not True)
-print (not False)
-print(num and num2)
-print(num or num2)
-input("enter Your Name: ")
-print("welcome", name)
-name= input("enter your name")
-age= int(input("enter your age"))
-marks= float(input("enter your marks"))
-print("welcome", name)
-print("age =",age)
-print("marks=", marks)
-numrr= int(input("first number"))
-nummr= int(input("second number"))
-print("sum=", nummr+numrr )
-side= int(input("enter your side :"))
-print("area", side*side)
-a= int(input("enter your side :"))
-b= int(input("enter your sid :"))
-print("avg=", (a+b)/2)
+#name= "Jyotirmoy Das Surjyo"
+#print("My Name is", name)
+#myage= 23
+#print("I am",myage,"years old")
+#fathername= "Topon Das"
+#print("My Fathers name is",fathername)
+#mothername= "Juthika Ghosh"
+#print("My Mothers name is",mothername)
+#fatherage=myage+35
+#print("My Father is", fatherage, "years old")
+#motherage=30+myage
+#print("My mother is",motherage,"years old")
+#print(type(name))
+#print(type(myage))
+#print(type(fatherage))
+#num=5
+#num2=6
+#print(num % num2) #remainder
+#print(num ** num2) #power
+#print(num / num2)
+#print(num == num2) #equal
+#print(num != num2) #not equal
+#print(num)
+#print (not True)
+#print (not False)
+#print(num and num2)
+#print(num or num2)
+#input("enter Your Name: ")
+#print("welcome", name)
+#name= input("enter your name")
+#age= int(input("enter your age"))
+#marks= float(input("enter your marks"))
+#print("welcome", name)
+#print("age =",age)
+#print("marks=", marks)
+#numrr= int(input("first number"))
+#nummr= int(input("second number"))
+#print("sum=", nummr+numrr )
+#side= int(input("enter your side :"))
+#print("area", side*side)
+#a= int(input("enter your side :"))
+#b= int(input("enter your sid :"))
+#print("avg=", (a+b)/2)
+str1= "this is a string"
+str2= 'jyotirmoy das surjyo'
+str3= """this is a string. \ti am create this string"""
+print(str3)
+print(len(str3))
+chr=str1[1:6]
+chrr=str3[-6:-1]
+print(chr)
+print(chrr)
+
